dded62c7be24d036bd3158a778a301a7ce6dccac
Alibi-stack <afox01155@gmail.com>
2025-09-27 16:30:59 +0500

    git commit -m "Added utility functions in the Utils class."

--- src/main/java/edu/Utils.java
+++ src/main/java/edu/Utils.java
@@ -4,17 +4,27 @@ package edu;
 import java.util.Random;
 
 public class Utils {
-    public static int[] randomArray(int n, long seed){
-        Random r = new Random(seed);
-        int[] a = new int[n];
-        for (int i=0;i<n;i++) a[i] = r.nextInt();
-        return a;
+    public static int[] randomArray(int size, long seed) {
+        Random rand = new Random(seed);
+        int[] array = new int[size];
+        for (int i = 0; i < size; i++) {
+            array[i] = rand.nextInt(1000);  // Random numbers between 0 and 999
+        }
+        return array;
     }
-    public static boolean isSorted(int[] a){
-        for (int i=1;i<a.length;i++) if (a[i-1] > a[i]) return false;
+
+    public static boolean isSorted(int[] array) {
+        for (int i = 1; i < array.length; i++) {
+            if (array[i - 1] > array[i]) {
+                return false;
+            }
+        }
         return true;
     }
-    public static void swap(int[] a, int i, int j){
-        int t = a[i]; a[i]=a[j]; a[j]=t;
+
+    public static void swap(int[] array, int i, int j) {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
     }
 }

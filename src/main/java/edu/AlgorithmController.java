1269962fe022ff0e878f2f0d6bfdc1ad72c7bede
Alibi-stack <afox01155@gmail.com>
2025-09-27 16:29:24 +0500

    git commit -m "Created AlgorithmController class to manage algorithm execution."

---
+++ src/main/java/edu/AlgorithmController.java
@@ -0,0 +1,29 @@
+
+import edu.MergeSortSimple;
+import edu.QuickSortSimple;
+import edu.Utils;
+import java.util.Scanner;
+
+public class AlgorithmController {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Choose an algorithm to run:");
+        System.out.println("1. MergeSort");
+        System.out.println("2. QuickSort");
+        int choice = scanner.nextInt();
+        int[] array = Utils.randomArray(1000, 1); // Example array
+
+        switch (choice) {
+            case 1:
+                System.out.println("Running MergeSort...");
+                MergeSortSimple.sort(array);
+                break;
+            case 2:
+                System.out.println("Running QuickSort...");
+                QuickSortSimple.sort(array, 2L);
+                break;
+            default:
+                System.out.println("Invalid choice.");
+        }
+    }
+}
